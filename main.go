ddcb91adf94ab010e0ebff7e79c114884ecd6699
moein9gh <moein9gh@gmail.com>
2022-03-04 15:15:16 +0330

    i created 100 txt files and wrote them 50000 lines using channel for speed test

--- main.go
+++ main.go
@@ -1,5 +1,96 @@
 package main
 
+import (
+	"errors"
+	"fmt"
+	"os"
+	"strconv"
+	"sync"
+	"time"
+)
+
+const (
+	AMOUNT_OF_LINES = 50000
+	AMOUNT_OF_FILES = 100
+	BY_CHANNEL      = iota
+	WITHOUT_CHANNEL
+)
+
+var wg = sync.WaitGroup{}
+
 func main() {
 
+	//uncomment this line if you wanna test without channel
+	// writeByChannel()
+
+	//uncomment this line if you wanna test by channel
+	// writeWithoutChannel()
+
+}
+
+//write to the files
+func writeToFile(file *os.File, i int, opType int) {
+	start := time.Now()
+
+	for i := 0; i < AMOUNT_OF_LINES; i++ {
+		file.WriteString("dummy line " + strconv.Itoa(i) + " \n")
+	}
+
+	duration := time.Since(start)
+
+	if opType == 0 {
+		wg.Done()
+	}
+
+	fmt.Println("job number "+strconv.Itoa(i)+" end ", duration)
+	time.Sleep(time.Millisecond * 1000)
+	file.Close()
+}
+
+//write file without using the channels
+func writeWithoutChannel() {
+	start := time.Now()
+
+	os.Mkdir("files", 0755)
+
+	for i := 0; i < AMOUNT_OF_FILES; i++ {
+		file, err := os.Create("files/test" + strconv.Itoa(i) + ".txt")
+		checkErr(err)
+
+		writeToFile(file, i, 1)
+
+	}
+
+	duration := time.Since(start)
+
+	fmt.Println("All tasks done without using channel in", duration)
+}
+
+//write file by using the channels
+func writeByChannel() {
+	start := time.Now()
+
+	os.Mkdir("files", 0755)
+
+	for i := 0; i < AMOUNT_OF_FILES; i++ {
+		file, err := os.Create("files/test" + strconv.Itoa(i) + ".txt")
+		checkErr(err)
+
+		go writeToFile(file, i, 0)
+
+		wg.Add(1)
+
+	}
+
+	wg.Wait()
+	duration := time.Since(start)
+
+	fmt.Println("All tasks done by using channel in", duration)
+}
+
+//error chacker
+func checkErr(err error) {
+	if err != nil {
+		errors.New("we have some problems")
+	}
 }
